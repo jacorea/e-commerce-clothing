03452cb557a47c164b58b663cf9593f0f71e4fc0
jcorea <jacorea410@gmail.com>
2019-09-10 10:08:30 -0700

    Add Header component, logo, and Links.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 import MenuItem from './components/menu-item/menu-item.component'
 
@@ -13,6 +14,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop/' component={ShopPage}/>

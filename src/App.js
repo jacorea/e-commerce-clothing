332addee9428a64fd573102e1630c1a8a44bbe11
jcorea <jacorea410@gmail.com>
2019-09-27 13:11:29 -0700

    restructured code so that authUser state gets stored in the app.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 
 
 import './App.css';
+import { create } from 'domain';
 
 
 class App extends React.Component {
@@ -26,8 +27,19 @@ class App extends React.Component {
   
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      this.setState({currentUser: userAuth });
     })
   }
 

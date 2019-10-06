f3dcfc03d4ab8a8f99e6fa9554f9064162f3a231
jcorea <jacorea410@gmail.com>
2019-10-06 11:45:48 -0700

    implemented userReducer and userActions to Header and Apps component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
 import HomePage from './pages/homepage/homepage.component'
@@ -8,29 +9,26 @@ import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import MenuItem from './components/menu-item/menu-item.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 import './App.css';
 
 
 class App extends React.Component {
-  constructor() {
-    super() 
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
   
   
 
   componentDidMount() {
+    const {setCurrentUser} = this.props
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
@@ -38,7 +36,7 @@ class App extends React.Component {
           })
         })
       }
-      this.setState({currentUser: userAuth });
+      setCurrentUser(userAuth);
     })
   }
 
@@ -60,4 +58,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user=> dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps )(App);

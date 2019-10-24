94b526ca6af8cdf3e4c8e54cf5beadb8f166089b
jcorea <jacorea410@gmail.com>
2019-10-24 12:06:44 -0700

    added toggleCartHidden functionality to improve UX when on checkout page.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,12 @@ import { withRouter } from 'react-router-dom';
 
 import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
+
 import './cart-dropdown.styles.scss';
 
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {cartItems.length ? (
@@ -21,7 +23,11 @@ const CartDropdown = ({ cartItems, history }) => (
             )
         }
         </div>
-        <CustomButton onClick ={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick ={()=> {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+            }}
+            >GO TO CHECKOUT</CustomButton>
     </div>
 )
 

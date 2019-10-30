21ef7d15d83e33b063d010f9df1a96629fb29443
jcorea <jacorea410@gmail.com>
2019-10-30 13:53:16 -0700

    add contactPage component and updated route to point to contact.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import CheckoutPage from './pages/checkout/checkout.component';
+import ContactPage from './pages/contact/contact.component';
 import MenuItem from './components/menu-item/menu-item.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { setCurrentUser } from './redux/user/user.actions';
@@ -57,6 +58,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
           <Route exact path='/checkout' component={CheckoutPage}/>
+          <Route exact path='/contact' component={ContactPage}/>
           <Route exact path='/signin' render={()=>this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
